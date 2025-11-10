3699440b314005093daa98fc25e3914b6c8cb986
Madadali Memon <madadalimemon90@gmail.com>
2025-11-10 08:50:30 -0800

    Update Delete.php login

--- Delete.php
+++ Delete.php
@@ -1,14 +1,9 @@
 <?php
 include "config.php";
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "DELETE FROM studect_table WHERE id = $id";
-    if ($conn->query($sql) === TRUE) {
-        header("Location: http://localhost/PHP_Ajax/index.php");
-        exit();
-    } else {
-        echo "Error deleting record: " . $conn->error;
-    }
+$stu_id = $_POST['id'];
+$sql = "DELETE FROM studect_table WHERE id = {$stu_id}";
+if (mysqli_query($conn, $sql)) {
+    echo 1;
+} else {
+    echo 0;
 }
-
-?>
